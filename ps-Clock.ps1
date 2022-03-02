508521613346b919b47bfc4be5c6681ae8304f16
Flint Gatrell <996640+flintg@users.noreply.github.com>
2022-03-01 19:16:53 -0700

    Code cleanup. Added colors and useful messaging. Arrgh!

--- ps-Clock.ps1
+++ ps-Clock.ps1
@@ -2,7 +2,6 @@
 [string]$appsettings = "WebClock,0.5.1,com.eagleflint.ps-clock";
 [string]$requestEndpoint = "";
 [string]$uri = "";
-[boolean]$isDefault = $false;
 [string]$version = "";
 [boolean]$success = $false;
 [string]$msg = "";
@@ -35,14 +34,14 @@ function Main {
 	Write-Host "msg:$($script:msg)";
 	
 	if ($script:success -eq $true) {
-		Write-Host "Login successful. Version $($script:version).";
+		Write-Host "Login successful. $($script:version)" -ForegroundColor Green;
 		GetOffset;
 		Write-Host "Ahoy! Ye be offset by $($script:GMTOffset) minutes.";
 		WebClock;
 	} elseif ($success -eq $false) {
 		Write-Host "There was a problem while logging in. The server reports the following error: $($script:msg)";
 		$retry = Read-Host -Prompt "Would you like to try again?  (yes/no)";
-		if ($retry -eq "yes") {
+		if ($retry.ToUpper().Substring(0,1) -eq "Y") {
 			Main;
 		}
 	} else {
@@ -55,7 +54,8 @@ function Main {
 }
 
 function WebClock {
-	$requestBody = [ordered]@{}
+	[Int32]$defaultChoice = 0;
+	$requestBody = [ordered]@{};
 
 	$requestEndpoint = "EPclkTable";
 	$uri = "$($script:settingsObject.url)$($requestEndpoint)";
@@ -84,63 +84,29 @@ function WebClock {
 	[int32]$tzOffset		= $script:GMTOffset;
 	[string]$clockTime		= [string]$rjson.EPclk.EPCLK_CLOCKDATE;
 	
-	[int32]$status = [int32]$json.EPclk.EPCLK_STATUS;
-	$isDefault = $false;
-	Write-Host "Choose a direction:";
-	Write-Host "===================";
-	for ($s=0;$s -le 1;$s++) {
-		if($s -eq $status) {
-			$isDefault = $true;
-		}
-		if ($s -ge 1) {
-			$displayName = "In";
-		} else {
-			$displayName = "Out";
-		}
-		if ($isDefault -eq $true) {
-			$displayName = "$($displayName) *";
-		}
-		Write-Host "$($s). $($displayName)";
-		$isDefault=$false;
-	}
-	Write-Host "===================";
-	$statusChoice = Read-Host "Direction";
-	if (-Not ([string]::IsNullOrWhiteSpace($statusChoice))) {
-		$status = [int32]$statusChoice;
-	}
+	[Int32]$status = [Int32]$rjson.EPclk.EPCLK_STATUS;
+	GetNewDirection -pStatus ([ref]$status);
 	
-	[int32]$earnID = [int32]$rjson.EPclk.EPCLK_PRITEMID;
+	[Int32]$earnID = [int32]$rjson.EPclk.EPCLK_PRITEMID;
 	if ($status -ge 1) {
 		$direction = "in";
 		[int32]$earning = 0;
 		if ($useEarning -eq 1) {
-			$isDefault = $false;
+			$defaultChoice = 0;
 			$earnings = $rjson.EPclk.EPclkItems;
+			Write-Host "";
 			Write-Host "Choose which earning item to use:";
 			Write-Host "=================================";
 			for ($e=0;$e -le $earnings.Count-1;$e++) {
-				if ($earnings.EPERN_PRITEMID[$e] -eq $rjson.EPclk.EPCLK_PRITEMID) {
-					$isDefault=$true;
-					[int32]$defaultItemID = [int32]$e;
-					[string]$defaultItemName = [string]$earnings.EPERN_ITEMNAME[$e];
-				}
-				$displayName = $earnings.EPERN_ITEMNAME[$e];
-				if ($isDefault -eq $true) {
-					$displayName = "$($displayName) *";
-				}
-				Write-Host "$($e). $($displayName)";
-				$isDefault=$false;
+				Write-Host "$($e). $($earnings.EPERN_ITEMNAME[$e])$(if($earnings.EPERN_PRITEMID[$e] -eq $earnID) {" *";$defaultChoice=$e;})";
 			}
 			Write-Host "=================================";
 			$earningChoice = Read-Host -Prompt "Earning item";
 			if ([string]::IsNullOrWhiteSpace($earningChoice)) {
-				$earning = [int32]$defaultItemID;
-			} else {
-				$earning = [int32]$earningChoice;
+				$earningChoice = $defaultChoice;
 			}
-			
-			$earnID = [int32]$earnings.EPERN_PRITEMID[$earning];
-			
+			$earning = [int32]$earningChoice;
+			$earnID = [Int32]$earnings.EPERN_PRITEMID[$earning];
 			$useMemo = [boolean]$earnings.EPERN_USEMEMO[$earning];
 			$usePosition = [boolean]$earnings.EPERN_USEPOSITION[$earning];
 			$useRate = [boolean]$earnings.EPERN_USERATE[$earning];
@@ -157,9 +123,10 @@ function WebClock {
 			$memo = Read-Host -Prompt "Memo";
 		}
 		
-		[int32]$position = 0;
+		[Int32]$fundingSourceID = 0;
 		if ($usePosition -eq $true) {
 			$positions = $rjson.EPclk.EPclkPositions;
+			Write-Host "";
 			Write-Host "Choose which Position Funding Source to use:";
 			Write-Host "============================================";
 			for ($p=0;$p -le $positions.Count-1;$p++) {
@@ -167,7 +134,7 @@ function WebClock {
 			}
 			Write-Host "============================================";
 			$positionChoice = Read-Host -Prompt "Position Funding Source";
-			$position = [int32]$positionChoice;
+			$fundingSourceID = [Int32]$positions.PCPOS_EMPLOYEEPOSITIONID[[int32]$positionChoice];
 		}
 		
 		[decimal]$rate = 0.00;
@@ -176,32 +143,22 @@ function WebClock {
 			$rate = [decimal]$rateEntry;
 		}
 		
-		[int32]$dist = 0;
+		[Int32]$distID = 0;
 		if ($useDistributionTable -eq $true) {
-			$isDefault = $false;
-			[int32]$defaultTableID = 0;
+			$defaultChoice = 0;
 			$dists = $rjson.EPclk.EPclkDists;
+			Write-Host "";
 			Write-Host "Choose which distribution table to use:";
 			Write-Host "=======================================";
 			for ($d=0;$d -le $dists.Count-1;$d++) {
-				if ($dists.GLDTB_DISTRIBUTIONTABLEID[$d] -eq $rjson.EPclk.EPCLK_DISTRIBUTIONTABLEID) {
-					$isDefault=$true;
-					$defaultTableID=[int32]$d;
-				}
-				$displayName = $dists.GLDTB_DISTRIBUTIONTABLE[$d];
-				if ($isDefault -eq $true) {
-					$displayName = "$($displayName) *";
-				}
-				Write-Host "$($d). $($displayName)"
-				$isDefault=$false;
+				Write-Host "$($d). $($dists.GLDTB_DISTRIBUTIONTABLE[$d])$(if($dists.GLDTB_DISTRIBUTIONTABLEID[$d] -eq $rjson.EPclk.EPCLK_DISTRIBUTIONTABLEID) {" *";$defaultChoice=$d})";
 			}
 			Write-Host "=======================================";
 			$distChoice = Read-Host -Prompt 'Distribution Table';
 			if ([string]::IsNullOrWhiteSpace($distChoice)) {
-				$dist = [int32]$defaultTableID;
-			} else {
-				$dist = [int32]$distChoice;
+				$distChoice = $defaultChoice;
 			}
+			$distID = [Int32]$dists.GLDTB_DISTRIBUTIONTABLEID[[Int32]$distChoice];
 		}
 		
 		[string]$mask = "";
@@ -217,32 +174,22 @@ function WebClock {
 			$amount = [decimal]$amountEntry;
 		}
 		
-		[int32]$type = 0;
+		[int32]$typeID = 0;
 		if ($useType -eq $true) {
-			$isDefault = $false;
-			[int32]$defaultTypeID = 0;
+			$defaultChoice = 0;
 			$types = $rjson.EPclk.EPclkTypes;
+			Write-Host "";
 			Write-Host "Choose which type to use:";
 			Write-Host "=========================";
 			for ($t=0;$t -le $types.Count-1;$t++) {
-				if ($types.AFTYP_TYPEID[$t] -eq $rjson.EPclk.EPCLK_TYPEID) {
-					$isDefault=$true;
-					$defaultTypeID=[int32]$t;
-				}
-				$displayName = $types.AFTYP_TYPE[$t];
-				if ($isDefault -eq $true) {
-					$displayName = "$($displayName) *";
-				}
-				Write-Host "$($t). $($displayName)"
-				$isDefault=$false;
+				Write-Host "$($t). $($types.AFTYP_TYPE[$t])$(if($types.AFTYP_TYPEID[$t] -eq $rjson.EPclk.EPCLK_TYPEID) {" *";$defaultChoice=$t})";
 			}
 			Write-Host "=========================";
 			$typeChoice = Read-Host -Prompt 'Type';
 			if ([string]::IsNullOrWhiteSpace($typeChoice)) {
-				$type = [int32]$defaultTypeID;
-			} else {
-				$type = [int32]$typeChoice;
+				$typeChoice = $defaultChoice;
 			}
+			$typeID = $types.AFTYP_TYPEID[[Int32]$typeChoice];
 		}
 		
 		[decimal]$hours = 0.00;
@@ -268,13 +215,13 @@ function WebClock {
 	$requestBody.Add("EPCLK_STATUS","$($status)");
 	$requestBody.Add("EPCLK_PRITEMID","$($earnID)");
 	if ($status -ge 1) {
-		$requestBody.Add("EPCLK_AMOUNT","$(if ($useAmount -eq $true){$amount}else{})");
-		$requestBody.Add("EPCLK_CLIENTID","$(if ($useClient -eq $true){$client}else{})"); # Not yet supported
-		$requestBody.Add("EPCLK_DISTRIBUTIONTABLEID","$(if ($useDistributionTable -eq $true){$rjson.EPclk.EPclkDists.GLDTB_DISTRIBUTIONTABLEID[$dist]}else{})");
-		$requestBody.Add("EPCLK_FUNDINGSOURCEID","$($rjson.EPclk.EPclkPositions.PCPOS_EMPLOYEEPOSITIONID[$position])");
+		$requestBody.Add("EPCLK_AMOUNT","$(if ($useAmount -eq $true){$amount})");
+		$requestBody.Add("EPCLK_CLIENTID","$(if ($useClient -eq $true){$client})"); # Not yet supported
+		$requestBody.Add("EPCLK_DISTRIBUTIONTABLEID","$(if ($useDistributionTable -eq $true){$distID})");
+		$requestBody.Add("EPCLK_FUNDINGSOURCEID","$(if($usePosition -eq $true) {$fundingSourceID})");
 		$requestBody.Add("EPCLK_MEMO","$($memo)");
-		$requestBody.Add("EPCLK_RATE","$(if($useRate -eq $true){$rate}else{})");
-		$requestBody.Add("EPCLK_TYPEID","$($rjson.EPclk.EPclkTypes.AFTYP_TYPEID[$type])");
+		$requestBody.Add("EPCLK_RATE","$(if($useRate -eq $true){$rate})");
+		$requestBody.Add("EPCLK_TYPEID","$(if($useType -eq $true){$typeID})");
 		$requestBody.Add("EPCLK_ACCOUNT","$($mask)");
 	}
 	
@@ -283,15 +230,15 @@ function WebClock {
 	$rjson = $response.Content | ConvertFrom-Json;
 	
 	if ($rjson.success -eq $true) {	
-		Write-Host "You were successfully clocked $($direction) at $($clockTime).";
+		Write-Host "Aye, Aye! Ye be clocked $($direction) at $($clockTime). $(if($($direction.ToUpper()) -eq "IN"){"Now swab the decks, ye scallywag!"}else{"Feel free to take a Caulk."})" -ForegroundColor Green;
 	} elseif ($rjson.success -eq $false) {
-		Write-Host "There was a problem while clocking you $($direction). The server reports the following error: $($rjson.msg)";
-		$retry = Read-Host -Prompt "Would you like to try again? (yes/no)";
-		if ($retry -eq "yes") {
+		Write-Host "Arr! There be a problem clocking you $($direction). The cap'n says: $($rjson.msg)." -ForegroundColor Red;
+		$retry = Read-Host -Prompt "Do ye fancy another try?" -ForegroundColor Yellow;
+		if ($retry.ToUpper().Substring(0,1) -eq "Y") {
 			WebClock;
 		}
 	} else {
-		Write-Host "Arr! There be a problem with ye request!  The cap'ns response be [$($response.StatusCode) - $($response.StatusDescription)]: $($response.Content)";
+		Write-Host "Arr! There be a problem with ye request!  The cap'n says [$($response.StatusCode) - $($response.StatusDescription)]: $($response.Content)" -ForegroundColor Red;
 	}
 }
 
@@ -348,22 +295,22 @@ function GetLocation {
 	[int32]$retryCount = 0;
 	[int32]$maxRetries = 10;
 	# From hxxps://stackoverflow.com/questions/46287792/powershell-getting-gps-coordinates-in-windows-10-using-windows-location-api/46287884 (accessed on 2019-11-18)
-	Add-Type -AssemblyName System.Device #Required to access System.Device.Location namespace
-	$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher #Create the required object
-	$GeoWatcher.Start() #Begin resolving current locaton
+	Add-Type -AssemblyName System.Device; #Required to access System.Device.Location namespace
+	$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher; #Create the required object
+	$GeoWatcher.Start(); #Begin resolving current locaton
 
 	while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied') -and ($retryCount -lt $maxRetries)) {
-		Start-Sleep -Milliseconds 100 #Wait for discovery.
+		Start-Sleep -Milliseconds 100; #Wait for discovery.
 		$retryCount = $retryCount + 1;
 	}
 
 	if ($GeoWatcher.Permission -eq 'Denied'){
-		Write-Host "Access Denied for Location Information";
+		Write-Host "Aye!! Good pirates don't be sharin' the locations of treasures." -ForegroundColor Yellow;
 	} else {
 		$script:Lat = [string]$GeoWatcher.Position.Location.Latitude;
 		$script:Lon = [string]$GeoWatcher.Position.Location.Longitude;
 	}
-	Write-Host "Ay matey! Yer reportin ye location as $($script:Lat) Latitude, $($script:Lon) Longitude.";
+	Write-Host "Aye, Matey! Yer reportin ye location as $($script:Lat) Latitude, $($script:Lon) Longitude.";
 }
 
 function GetDeviceID {
@@ -409,7 +356,7 @@ function SaveSettings {
 function RequestURL {
 	$entryURL = Read-Host -Prompt "URL (e.g. https://clock.example.com/)";
 	if ([string]::IsNullOrWhiteSpace($entryURL)) {
-		Write-Host "Arr! Walk yer fingers, or ye be walkin' the plank!"
+		Write-Host "Arr! Walk yer fingers, or ye be walkin' the plank!";
 		RequestURL; #URL is required
 	} else {
 		if (-Not ($entryURL.substring($entryURL.length - 1, 1) -eq "/")) {
@@ -423,7 +370,7 @@ function RequestURL {
 function RequestUser {
 	$entryUser = Read-Host -Prompt "Username";
 	if ([string]::IsNullOrWhiteSpace($entryUser)) {
-		Write-Host "Arr! Walk yer fingers, or ye be walkin' the plank!"
+		Write-Host "Arr! Walk yer fingers, or ye be walkin' the plank!";
 		RequestUser; #User is required
 	} else {
 		$script:settingsObject.user = [string]$entryUser;
@@ -432,7 +379,7 @@ function RequestUser {
 }
 
 function RequestSavePassword {
-	$entrySavePwd = Read-Host -Prompt "Save password for easier login (Yes / No)?";
+	$entrySavePwd = Read-Host -Prompt "Save password for easier login? (Y/N)";
 	if ([string]::IsNullOrWhiteSpace($entrySavePwd)) {
 		RequestSavePassword;
 	} else {
@@ -441,10 +388,11 @@ function RequestSavePassword {
 			$script:settingsObject.savepwd = $true;
 			$script:settingsChanged = $true;
 		} elseif ($entrySavePwd.Substring(0,1) -eq "N") {
+			Write-Host "Ye can't be sayin' I didn't warn ya!" -ForegroundColor Green;
 			$script:settingsObject.savepwd = $false;
 			$script:settingsChanged = $true;
 		} else {
-			Write-Host "Arr! That be the wrong answer, matey! [$($entrySavePwd)]";
+			Write-Host "Arr! That be the wrong answer, matey! [$($entrySavePwd)] Do it right, or ye'll be flogged!" -ForegroundColor Red;
 			RequestSavePassword;
 		}
 	}
@@ -459,11 +407,36 @@ function RequestPassword {
 function RequestDB {
 	$entryDB = Read-Host -Prompt "Database";
 	if ([string]::IsNullOrWhiteSpace($entryDB)) {
-		$entryDB = "<no database>"
-		Write-Host "Ay! I suppose there be no persuadin' ya."
+		$entryDB = "<no database>";
+		Write-Host "Aye!! I suppose there be no persuadin' ya." -ForegroundColor Green;
 	}
 	$script:settingsObject.db = [string]$entryDB;
 	$script:settingsChanged = $true;
+}
+
+function GetNewDirection ([ref]$pStatus) {
+	[Int32]$defaultDir=$(if ($pStatus.Value -eq 0){1}else{0});
+	Write-Host "";
+	Write-Host "Choose a direction:";
+	Write-Host "===================";
+	for ([Int32]$s=0;$s -le 1;$s++) {
+		switch ($s){
+			0	{Write-Host "$($s). Out$(if ($s -eq $defaultDir){" *"})"}
+			1	{Write-Host "$($s). In $(if ($s -eq $defaultDir){" *"})"}
+		}
+	}
+	Write-Host "===================";
+	$statusChoice = Read-Host "Direction";
+	if (-Not ([string]::IsNullOrWhiteSpace($statusChoice))) {
+		if (([Int32]$statusChoice -eq 0) -or ([Int32]$statusChoice -eq 1)) {
+			$pStatus.Value = [int32]$statusChoice;
+		} else {
+			Write-Host "Arr! Ye have only two options! Try again, or walk the plank!" -ForegroundColor Red;
+			GetNewDirection [ref]$pStatus;
+		}
+	} else {
+		$pStatus.Value = $defaultDir;
+	}
 }
 
 Main;
